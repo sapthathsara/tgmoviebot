6497083611d9d775c52319a6021fca8b070dc9cf
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-01-04 19:25:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -791,7 +791,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[🎬🎉{get_size(file.file_size)}] 📁🎉{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🎬🎉[{get_size(file.file_size)}]📁🎉 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
