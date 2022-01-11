6bdcd8c3d46aa310a4760ef6fca640441d1cc7db
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-01-11 15:44:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -15,14 +15,17 @@ class Script(object):
 Hey How's the features Did you Like it <a href='tg://user?id=1951205538'><b>𝑹𝒂𝒕𝒆 𝒊𝒕 𝒉𝒆𝒓𝒆 </a></b>"""
 
     ABOUT_TXT = """<b>➥ My name: {}
-➪ 𝑪𝒓𝒆𝒂𝒕𝒆𝒓: <a href='tg://user?id=1951205538'><b>༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒</b></a>
-➪ 𝑪𝒓𝒆𝒅𝒊𝒕𝒔 : <a href='https://t.me/Abhisheksvlog'>Everyone in this journey </a>
-➪ 𝑳𝒊𝒃𝒓𝒂𝒓𝒚 :<a href='https://docs.pyrogram.org/'>𝑷ʏʀᴏɢʀᴀᴍ </a>
-➪ 𝑳𝒂𝒏𝒈𝒖𝒂𝒈𝒆 : <a href='https://docs.pyrogram.org/'>𝑷𝒚𝒕𝒉𝒐𝒏 3 </a>
-➪ 𝑫𝒂𝒕𝒂 𝑩𝒂𝒔𝒆 : <a href='https://www.mongodb.com/'>𝑴𝒐𝒏𝒈𝒐 𝑫𝑩</a>
-➪ 𝑩𝒐𝒕 𝑺𝒆𝒓𝒗𝒆𝒓 : <a href='https://dashboard.heroku.com/apps'>𝑯𝒆𝒓𝒐𝒌𝒖</a>
-➪ 𝑺𝒐𝒖𝒓𝒄𝒆 𝑪𝒐𝒅𝒆 :<a href='https://t.me/cinemaenglish123'>𝑪𝒍𝒊𝒄𝒌 𝑯𝒆𝒓𝒆</a>
-➪ 𝑩𝒖𝒊𝒍𝒅 𝑺𝒕𝒂𝒕𝒖𝒔 : <a href='https://t.me/TigerShroffimdbot'>𝒗1.0.1[𝑩𝑬𝑻𝑨]</a>
+╔══════════════════❍
+║╭━━━━━━━━━━━━━━━➣ 
+║➠ 𝙼𝚈 𝙽𝙰𝙼𝙴 -  Tiger Shroff
+║➠ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 - :<a href='tg://user?id=1951205538'><b>༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒</b></a>
+║➠ 𝙲𝚁𝙴𝙳𝙸𝚃𝚂 - <a href='https://t.me/Abhisheksvlog'>Everyone in this journey </a>
+║➠ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 - <a href='https://docs.pyrogram.org/'>𝑷𝒚𝒕𝒉𝒐𝒏 3 </a>
+║➠ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 - <a href='https://docs.pyrogram.org/'>𝑷ʏʀᴏɢʀᴀᴍ </a>
+║➠ 𝙲𝙻𝙾𝙽𝙴𝙳 𝙵𝚁𝙾𝙼 - EvaMaria
+║➠ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 -  <a href='https://dashboard.heroku.com/apps'>𝑯𝒆𝒓𝒐𝒌𝒖</a>
+║➠ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂 - <a href='https://t.me/TigerShroffimdbot'>𝒗1.0.1[𝑩𝑬𝑻𝑨]</a>
+║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍
 
 
 🔖 𝑸𝒖𝒐𝒕𝒆 : ആരും പേടിക്കേണ്ട എല്ലാവർക്കും കിട്ടും ™️"""
