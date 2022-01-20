4756dbc2ec0ea276fbdb45948f622a40759d36b6
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-01-20 11:23:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -703,7 +703,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.CORONA_TXT,
+            text=Script.CORONA_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -713,7 +713,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.STICKER_TXT,
+            text=Script.STICKER_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
